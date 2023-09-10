733f23518942435e7fc337690080fae9bd682719
Salif Tankoano <tanksalif@gmail.com>
2023-09-10 14:20:00 -0400

    update intro section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import './App.css';
 // import CTPImg from './assets/logoq.png';
 import Navbar from './components/Navbar/Navbar';
-import Intro from './components/Intro/intro';
-import Skills from './components/Skills/skills';
-import Projects from './components/projects/projects';
+import Intro from './components/Intro/Intro';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/footer';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./intro.css";
-import bg from "../../assets/image.png";
 import btnImg from "../../assets/hireMe.svg";
 import Resume from "../../assets/resume.pdf";
 
@@ -8,12 +7,10 @@ const intro= ()=>{
     return (
         <section id="intro">
             <div className="intro-content">
-                <span className="hello">Hello,</span>
                 <span className="intro-txt">I'm <span className="intro-name">Salif</span><br/>Junior Software Developer</span>
-                <span className="intro-para">Looking for my first professional position.<br/> I specialize in building intuitive Software and genuine relationships!</span>
+                <span className="intro-para">Fall 2023 Grad<br/>Looking for my first professional position.<br/> I specialize in building intuitive Software and genuine relationships!</span>
                 <a  href={Resume} download="salif-resume"><button className="btn"><img src={btnImg} alt="suitcase logo" className="btn-img" />Hire me!</button></a>
             </div>
-            <img src={bg} alt="profile" className="bg"/>
         </section>
     )
 }
