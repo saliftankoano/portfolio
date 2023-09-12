ea77c3d515d60ccf0cbe24041cfadde99fd70941
Salif Tankoano <tanksalif@gmail.com>
2023-09-11 23:51:43 -0400

    update project section

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./projects.css";
-import Portfolio1 from "../../assets/portfolio-1.png";
-import Portfolio2 from "../../assets/portfolio-2.png";
-import Portfolio3 from "../../assets/portfolio-3.png";
-import Portfolio4 from "../../assets/portfolio-4.png";
-import Portfolio5 from "../../assets/portfolio-5.png";
-import Portfolio6 from "../../assets/portfolio-6.png";
+import Portfolio1 from "../../assets/perfect-fit.png";
+import Portfolio2 from "../../assets/simon.png";
+import Portfolio3 from "../../assets/meme-parlor.png";
 
 const projects= ()=>{
     return (
@@ -13,12 +10,9 @@ const projects= ()=>{
             <h2 className="projects-title">My Projects</h2>
             <span className="projects-desc">I strive to build original projects with a plethora of purposes.<br/>These projects are used by friends, family and more.<br/> Thank you all, for your helping me improve my creations!</span>
             <div className="projects-imgs">
-                <img src={Portfolio1} className="projects-img" alt=""/>
-                <img src={Portfolio2} className="projects-img" alt=""/>
-                <img src={Portfolio3} className="projects-img" alt=""/>
-                <img src={Portfolio4} className="projects-img" alt=""/>
-                <img src={Portfolio5} className="projects-img" alt=""/>
-                <img src={Portfolio6} className="projects-img" alt=""/>
+                <a href="https://xchen601.github.io/Fashion-App/" target="_blank"><img src={Portfolio1} className="projects-img" alt="perfect fit project"/></a>
+                <a href="saliftankoano.github.io/simon-game/" target="_blank"><img src={Portfolio2} className="projects-img" alt="simon game"/></a>
+                <a href="https://saliftankoano.github.io/meme-parlor/" target="_blank"><img src={Portfolio3} className="projects-img" alt=""/></a>
             </div>
             <button className="projects-btn">See More</button>
         </section>
