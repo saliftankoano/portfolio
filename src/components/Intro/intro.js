b9fec42ecf51c2a8aa91cdab0a77796b51f767e3
Salif Tankoano <tanksalif@gmail.com>
2023-09-17 19:40:25 -0400

    change intro banner, navbar and skills sections

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./intro.css";
 import btnImg from "../../assets/hireMe.svg";
 import Resume from "../../assets/resume.pdf";
+import profile from "../../assets/me.png";
 
-const intro= ()=>{
+const Intro= ()=>{
     return (
-        <section id="intro">
-            <div className="intro-content">
-                <span className="intro-txt">I'm <span className="intro-name">Salif</span><br/>Junior Software Developer</span>
-                <span className="intro-para">Fall 2023 Grad<br/>Looking for my first professional position.<br/> I specialize in building intuitive Software and genuine relationships!</span>
-                <a  href={Resume} download="salif-resume"><button className="btn"><img src={btnImg} alt="suitcase logo" className="btn-img" />Hire me!</button></a>
-            </div>
+        <section className="intro">
+                <img src={profile} alt='profile' id='profile'/>
+                <div className='left'>
+                    <span className="intro-name">I'm <span className="salif">Salif</span><br/></span>
+                    <span className="job">Junior Software Developer<br/></span>
+                    <a  href={Resume} download="salif-resume"><button className="btn"><img src={btnImg} alt="suitcase logo" className="btn-img" />Resume</button></a>
+                </div>
         </section>
-    )
+    );
 }
-export default intro;
+export default Intro;
